f00b6cf9001258f3c0658e2fdb2140e93f6c80e5
gabonbon0528 <judy860528@gmail.com>
2025-04-01 23:40:01 +0800

    feat: ingredient drawer

--- components/IngredientDrawer.tsx
+++ components/IngredientDrawer.tsx
@@ -1,7 +1,38 @@
 "use client";
 
-import { Button, CloseButton, Drawer, Portal } from "@chakra-ui/react";
+import {
+  Button,
+  CloseButton,
+  Drawer,
+  Flex,
+  Portal,
+  RadioCard,
+} from "@chakra-ui/react";
 import { useState } from "react";
+
+const items = [
+  {
+    value: "1",
+    title: "阿嘟高筋麵粉",
+    description: "1000g",
+    cost: 100,
+    createdAt: "2021-01-01",
+  },
+  {
+    value: "2",
+    title: "紫羅蘭高筋麵粉",
+    description: "1000g",
+    cost: 100,
+    createdAt: "2021-01-01",
+  },
+  {
+    value: "3",
+    title: "小白高筋麵粉",
+    description: "1000g",
+    cost: 100,
+    createdAt: "2021-01-01",
+  },
+];
 
 export default function IngredientDrawer() {
   const [open, setOpen] = useState(false);
@@ -18,13 +49,27 @@ export default function IngredientDrawer() {
         <Drawer.Positioner>
           <Drawer.Content>
             <Drawer.Header>
-              <Drawer.Title>Drawer Title</Drawer.Title>
+              <Drawer.Title>選擇原料</Drawer.Title>
             </Drawer.Header>
             <Drawer.Body>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+              <RadioCard.Root defaultValue="next">
+                <Flex flexDirection={"column"} gap={2}>
+                  {items.map((item) => (
+                    <RadioCard.Item key={item.value} value={item.value}>
+                      <RadioCard.ItemHiddenInput />
+                      <RadioCard.ItemControl>
+                        <RadioCard.ItemContent>
+                          <RadioCard.ItemText>{item.title}</RadioCard.ItemText>
+                          <RadioCard.ItemDescription>
+                            {item.description} / {item.cost}元 / {item.createdAt}
+                          </RadioCard.ItemDescription>
+                        </RadioCard.ItemContent>
+                        <RadioCard.ItemIndicator />
+                      </RadioCard.ItemControl>
+                    </RadioCard.Item>
+                  ))}
+                </Flex>
+              </RadioCard.Root>
             </Drawer.Body>
             <Drawer.Footer>
               <Button variant="outline">Cancel</Button>
